7a67cc582c4f23635c9015eaef6fc3c80d817c4b
25LP5320 25LP5320 <25LP5320@G302-CC0093.local>
2026-01-29 20:10:46 +0800

    eslint fix

--- front-end/eslint.config.mjs
+++ front-end/eslint.config.mjs
@@ -1,26 +1,48 @@
-module.exports = {
-  root: true,
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: 6,
-    sourceType: "module",
+// module.exports = {
+//   root: true,
+//   parser: "@typescript-eslint/parser",
+//   parserOptions: {
+//     ecmaVersion: 6,
+//     sourceType: "module",
+//   },
+//   plugins: ["@typescript-eslint"],
+//   rules: {
+//     "@typescript-eslint/naming-convention": "warn",
+//     "@typescript-eslint/semi": "warn",
+//     curly: "warn",
+//     eqeqeq: "warn",
+//     "no-throw-literal": "warn",
+//     semi: "off",
+//     "@typescript-eslint/no-unused-vars": [
+//       "error",
+//       {
+//         argsIgnorePattern: "^_", // Ignore parameters starting with underscore
+//         varsIgnorePattern: "^_", // Ignore variables starting with underscore
+//         caughtErrorsIgnorePattern: "^_", // Ignore caught errors starting with underscore
+//       },
+//     ],
+//   },
+//   ignorePatterns: ["out", "dist", "**/*.d.ts"],
+// };
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+        },
+      ],
+    },
   },
-  plugins: ["@typescript-eslint"],
-  rules: {
-    "@typescript-eslint/naming-convention": "warn",
-    "@typescript-eslint/semi": "warn",
-    curly: "warn",
-    eqeqeq: "warn",
-    "no-throw-literal": "warn",
-    semi: "off",
-    "@typescript-eslint/no-unused-vars": [
-      "error",
-      {
-        argsIgnorePattern: "^_", // Ignore parameters starting with underscore
-        varsIgnorePattern: "^_", // Ignore variables starting with underscore
-        caughtErrorsIgnorePattern: "^_", // Ignore caught errors starting with underscore
-      },
-    ],
+  {
+    ignores: ["out/**", "dist/**", "**/*.d.ts"],
   },
-  ignorePatterns: ["out", "dist", "**/*.d.ts"],
-};
+];
