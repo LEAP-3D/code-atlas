149837ce31a24a14060179ef37a4945ddc206a30
25LP5320 25LP5320 <25LP5320@G302-CC0093.local>
2026-01-29 19:52:52 +0800

    test code yvdag

--- front-end/eslint.config.mjs
+++ front-end/eslint.config.mjs
@@ -1,27 +1,26 @@
-import { defineConfig } from "eslint/config";
-import tseslint from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
-
-export default defineConfig([
-  {
-    files: ["**/*.ts"],
-    languageOptions: {
-      parser: tsParser,
-      parserOptions: {
-        project: "./tsconfig.json",
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/naming-convention": "warn",
+    "@typescript-eslint/semi": "warn",
+    curly: "warn",
+    eqeqeq: "warn",
+    "no-throw-literal": "warn",
+    semi: "off",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        argsIgnorePattern: "^_", // Ignore parameters starting with underscore
+        varsIgnorePattern: "^_", // Ignore variables starting with underscore
+        caughtErrorsIgnorePattern: "^_", // Ignore caught errors starting with underscore
       },
-    },
-    plugins: {
-      "@typescript-eslint": tseslint,
-    },
-    rules: {
-      "@typescript-eslint/no-unused-vars": "error",
-      "@typescript-eslint/no-explicit-any": "error",
-      "prefer-const": "error",
-      eqeqeq: ["error", "always"],
-    },
+    ],
   },
-  {
-    ignores: ["out/**", "node_modules/**"],
-  },
-]);
+  ignorePatterns: ["out", "dist", "**/*.d.ts"],
+};
